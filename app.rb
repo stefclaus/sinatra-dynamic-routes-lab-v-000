4e2e7bd245bf69fddd17d108f1fd626be7010f47
Stef Claus <stef.claus@gmail.com>
2019-03-03 21:55:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
 
   get '/:number/:phrase' do
     # returns the phrase the number of times it is in the paramater
-    "#{params[:phrase] * params[:number].to_i}"
+    "#{params[:phrase] " " * params[:number].to_i}"
     #"#{params[:phrase] until params[:phrase] == params [:number]}"
   end
 
