952f74f903f3a1f10bdc461dae000c45f163c3db
Stef Claus <stef.claus@gmail.com>
2019-03-03 21:31:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,8 @@ class App < Sinatra::Base
   end
 
   get '/square/:number' do
-    "#{params[:number].to_i.square!}"
+    # return the number squared
+    "#{params[:number].to_i ** 2}"
   end
 
 end
