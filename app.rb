ef3f21299127e6cc7ea6500b073deb78e4a09985
Stef Claus <stef.claus@gmail.com>
2019-03-03 21:33:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ class App < Sinatra::Base
   end
 
   get '/:number/:phrase' do
-    # return the number squared
-    "#{params[:number].to_i ** 2}"
+    # returns the phrase the number of times it is in the paramater
+    do "#{params[:phrase] until params[:phrase] == params [:number]}"
   end
 end
