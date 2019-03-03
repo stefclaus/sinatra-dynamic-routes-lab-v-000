8bbe5f35bf374ba6e75ad2759727c1bedec314b3
Stef Claus <stef.claus@gmail.com>
2019-03-03 21:41:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    "#{params[:number1].to_i params[:operation] params[:number2].to_i}"
+    "#{params[:number1].to_i + params[:number2].to_i}"
   end
 
 
